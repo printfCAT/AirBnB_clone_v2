2a2d83245dbc947d00e36040b0a2890e1600f126
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-21 12:53:44 -0700

    task 8 fix attempt

--- models/place.py
+++ models/place.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.user import User
 from models.city import City
 from os import getenv
 import models
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel):
@@ -24,19 +32,7 @@ class Place(BaseModel):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
+        user = relationship("User", back_populates="place")
+        cities = relationship("City", back_populates="place",
+                              cascade="all, delete")
         amenity_ids = []
-    else: 
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
-    user = relationship("User", back_populates="places")
-    cities = relationship("City", back_populates="places", cascade="all, delete")
