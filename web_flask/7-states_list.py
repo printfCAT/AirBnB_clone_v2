4fd60d1cc68a2de043eb927d39c785d210a8a302
printfCAT <cliffomanyo@gmail.com>
2023-07-23 14:42:05 -0700

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" define a function """
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ Displays a list of all State objects in DBStorage sorted by name """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ remove the current SQLAlchemy Session after each request """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """ main function """
+    app.run(host="0.0.0.0", port=5000)
